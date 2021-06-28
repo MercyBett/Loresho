7cc3c8b157a81a5a5c820ce98b30ac4be961e3f9
Mercybett <merjep551@gmail.com>
2021-06-28 16:26:41 +0300

    add CRUD functionality to product

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('product-list/', views.product_list, name="product_list"),
+    path('product-detail/<str:pk>', views.product_detail, name="product_detail"),
+    path('product-create/', views.product_create, name="product_create"),
+    path('product-update/<str:pk>', views.product_update, name="product_update"),
+    path('product-delete/<str:pk>', views.product_delete, name="product_delete"),
 
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,49 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from .models import Product
 # Create your views here.
+
+
 @api_view(['GET'])
 def home(request):
-  return Response("API BASE POINT",)
+    return Response("API BASE POINT")
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    products = Product.objects.get(id=pk)
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def product_create(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def product_update(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def product_delete(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response("Product deleted successfully!")
